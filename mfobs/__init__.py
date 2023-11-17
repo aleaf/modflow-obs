f4e0f0f7ac79d5784ddcd658733ef436eeefa777
Leaf, Andrew T <aleaf@usgs.gov>
2023-11-17 14:00:39 -0600

    update versioneer

--- mfobs/__init__.py
+++ mfobs/__init__.py
@@ -1,7 +1,6 @@
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
 
 from mfobs.heads import get_head_obs
 from mfobs.obs import get_spatial_differences, get_temporal_differences, get_base_obs
